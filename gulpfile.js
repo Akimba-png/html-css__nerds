eed524afe86b83ea0e23b9a67a0c778edc386db5
Akimba-png <akimba@gmail.com>
2022-01-05 18:49:35 +0300

    Добавляет стилизацию header. Добавляет вёрстку и стилизацию footer. Добавляет в gulp file скрипт по созданию SVG спрайта.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const svgSprite = require('gulp-svg-sprite');
 
 
 const styles = () => {
   return gulp.src('./source/css/**/*.css')
     .pipe(browserSync.stream());
 };
+
+const createSprite = () => {
+  return gulp.src('./source/img/icons/*.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../sprite.svg",
+      }
+    }
+  }))
+  .pipe(gulp.dest('./source/img/icons'));
+}
+exports.sprite = createSprite;
+
 
 const server = (done) => {
   browserSync.init({
@@ -21,4 +36,4 @@ const watcher = () => {
   gulp.watch('./source/*.html').on('change', browserSync.reload);
 };
 
-exports.default = gulp.series(server, watcher);
+exports.default = gulp.series(createSprite, server, watcher);
