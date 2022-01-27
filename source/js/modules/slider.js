97a22c9b74e6877d940c572a3417d1922b849d0b
Akimba-png <akimba@gmail.com>
2022-01-27 17:22:43 +0300

    Рефакторит и завершает вёртску формы и блока фильтрации

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,19 +1,23 @@
-const sliderElement = document.querySelector(".slider");
-const toggleContainerElement = sliderElement.querySelector(".slider__toggles");
-const sliderItemElements = sliderElement.querySelectorAll(".slider__item");
-const toggleElements = toggleContainerElement.children;
+const PAGE_INDEX_ROUTE = '/index.html';
+const currentRoute = window.location.pathname;
 
+if (currentRoute === PAGE_INDEX_ROUTE) {
+  const sliderElement = document.querySelector(".slider");
+  const toggleContainerElement = sliderElement.querySelector(".slider__toggles");
+  const sliderItemElements = sliderElement.querySelectorAll(".slider__item");
+  const toggleElements = toggleContainerElement.children;
 
-function onToggleClick({target}) {
-  if (target.matches(".slider__toggle--current")) {
-    return;
-  }
-  if(target.matches(".slider__toggle")) {
-    const index = [...toggleElements].indexOf(target);
-    sliderElement.querySelector(".features__item--current").classList.remove("features__item--current");
-    sliderElement.querySelector(".slider__toggle--current").classList.remove("slider__toggle--current");
-    sliderItemElements[index].classList.add("features__item--current");
-    target.classList.add("slider__toggle--current");
-  }
-};
-toggleContainerElement.addEventListener("click", onToggleClick);
+  function onToggleClick({target}) {
+    if (target.matches(".slider__toggle--current")) {
+      return;
+    }
+    if(target.matches(".slider__toggle")) {
+      const index = [...toggleElements].indexOf(target);
+      sliderElement.querySelector(".features__item--current").classList.remove("features__item--current");
+      sliderElement.querySelector(".slider__toggle--current").classList.remove("slider__toggle--current");
+      sliderItemElements[index].classList.add("features__item--current");
+      target.classList.add("slider__toggle--current");
+    }
+  };
+  toggleContainerElement.addEventListener("click", onToggleClick);
+}
